c4a3a26b4901f28b547695dea18c30fb7682aa00
Barrett Simms <wbsimms@gmail.com>
2013-03-23 15:53:29 -0400

    Explicit constructors and private methods

--- ClassConstructors/ClassConstructors/ClassConstructors.cpp
+++ ClassConstructors/ClassConstructors/ClassConstructors.cpp
@@ -7,22 +7,27 @@ using namespace std;
 class CBox
 {
 	public:
-		double m_Length, m_Width, m_Height;
 		double Volume();
-		CBox(double length, double width, double height)
+		double GetLength();
+		explicit CBox(double length = 0.0, double width = 1.0, double height = 1) : 
+			m_Length(length), m_Width(width), m_Height(height)
 		{
-			m_Height = height;
-			m_Length = length;
-			m_Width = width;
 		}
 
-		CBox()
+		CBox(double length) : 
+			m_Length(length), m_Width(length), m_Height(length)
 		{
-			m_Height = 0;
-			m_Length = 0;
-			m_Width = 0;
 		}
+
+	private:
+		double m_Length, m_Width, m_Height;
 };
+
+inline double CBox::GetLength()
+{
+	return this->m_Length;
+}
+
 
 inline double CBox::Volume()
 {
@@ -32,9 +37,11 @@ inline double CBox::Volume()
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CBox box(1.1,2.2,3.3);
-	CBox box2;
+	CBox box2 = 99.99;
+	
 	cout << box.Volume() << endl;
 	cout << box2.Volume() << endl;
+	cout << box2.GetLength() << endl;
 	return 0;
 }
 
